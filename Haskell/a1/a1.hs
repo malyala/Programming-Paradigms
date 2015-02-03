0fc13e944d795169b89aadbf6470d05c6802b7b2
Keith Weaver <keithmweaver@gmail.com>
2015-02-03 11:48:17 -0500

    had commas in the functions

--- Haskell/a1/a1.hs
+++ Haskell/a1/a1.hs
@@ -7,13 +7,13 @@ isPerfect 0 = False -- if 0 returns false
 isPerfect n
 	| mod n 2 == 1 = False -- if odd return false
 	| n < 0 = False -- if less than 0 than return false
-	| (2 * n) == findFactor (n, n, 0) = True -- if 2 times n is equal the sum of the factors is equal to true
+	| (2 * n) == findFactor (n n 0) = True -- if 2 times n is equal the sum of the factors is equal to true
 	| otherwise = False -- other than that, false
 
 findFactor :: Int -> Int -> Int -> Int -- finds the sum, by going through the factors recursively
 findFactor start n total
 	| n < 1 = False
-	| otherwise total = findFactor(start, n, total)
+	| otherwise total = findFactor(start n total)
 		| (mod start n == 0) then total + n
 
 nthTrue :: (Int -> Bool) -> Int -> Int -- passes in the function (that has a int and boolean) and int, returns int
