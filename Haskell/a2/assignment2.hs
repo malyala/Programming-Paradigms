1769f10e014465b286cce475aa3c2e60baae99d3
Keith Weaver <keithmweaver@gmail.com>
2015-02-18 20:58:04 -0500

    enough for tonight, it is reading week.

--- Haskell/a2/assignment2.hs
+++ Haskell/a2/assignment2.hs
@@ -1,3 +1,28 @@
+module assignment2 where
 -- Keith Weaver
 -- CISC 260 - A2
 -- 10048505
+
+-- finding all paths
+--case 1: starting at 1, jump 2 everytime
+--case 2: starting at 1, jump 2 everytime except at n-1 item (goes from end to second item)
+--example:
+--74596123
+--C1: 7562
+--C2: (very last item -> 2) 7563 
+--	  (second last item ->6) 7513 (1 to 3 is 2)
+--	  (third last item -> 5) 7913 (jumped 5)
+--
+--case 3: jump 3 everytime
+--case 4: jump 3 except at n-1 starting at very last element of case 3
+--
+--
+--
+--
+--
+--
+--
+
+hopscotch :: [Int] -> ([Int], Int)
+hopscotch [] = ([], 0)
+hopscotch currentl = 
