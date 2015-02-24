44d7a5a12cd45d1439856cb633bf69f293518e3e
Keith Weaver <keithmweaver@gmail.com>
2015-02-24 11:35:26 -0500

    what i have in sudo, I just have to figure out the syntax

--- Haskell/a2/assignment2.hs
+++ Haskell/a2/assignment2.hs
@@ -1,4 +1,4 @@
-module assignment2 where
+--module assignment2 where
 -- Keith Weaver
 -- CISC 260 - A2
 -- 10048505
@@ -17,12 +17,45 @@ module assignment2 where
 --case 4: jump 3 except at n-1 starting at very last element of case 3
 --
 --
+-- sudo to find all paths 
+-- paths :: [Int] -> [[Int]]
+-- paths :: list = []
+-- paths :: list
+-- 		  return [helperPaths(list len(list) [] 2 3) : helperPaths(list len(list) [] 3 2)] <-- no idea is what an add is as of now
+-- 
+-- helpPaths :: list n currentResults jump jump2
+-- 			if n = 0 then return currentResults
+-- 			else
+--  		    path = singlePath(list n 0 [] jump jump2)
+--				add path to current results
+-- 				helperPaths(list (n - jump) currentResults)
+-- 
+-- singlePath :: list n i path jump jump2
+-- 				if n == len(list) - 1 || n == len(list) - 2 = return [list[i]] <- last element
+-- 				else
+--					if n==i then
+--						return [ list[i] : singlePath(list n (i+jump2) path jump jump2)] <-- if you are working with jumps of all 2s then adding paths with one element jumping 3
+--					else
+--						return [ list[i] : singlePath(list n (i+jump) path jump jump2)]
+-- 
+-- bestPath allPathsList = (z, sum(z))
+-- 			where
+--				z = bestPathsHelper(allPathsList len(allPathsList) 0)
+-- 
+-- bestPathsHelper :: allPathsList numList count
+-- 					if count == (numList-1) then return allPathsList[count]
+--					else: 
+--						|x > y = return z
+--						|x <= y = return allPathsList(count)
+-- 						where
+--							z = allPathsList(numList count+1)
+-- 							y = sum(allPathsList[count])
+--							x = sum(z)
 --
+--above assumes 1 different type of jump, so i may need to add a variety of sizes but it works for her example
+--	example: 2 jump 3 jump  3 jump 2
+-- also assumes : is add two list or add an element to a list
 --
---
---
---
-
-hopscotch :: [Int] -> ([Int], Int)
-hopscotch [] = ([], 0)
-hopscotch currentl = 
+--hopscotch :: [Int] -> ([Int], Int)
+--hopscotch [] = ([], 0)
+--hopscotch currentl = 
