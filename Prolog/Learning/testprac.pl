20f091e6b13b8279b528cd3f8f1e56fb8d381d9e
Keith Weaver <keithmweaver@gmail.com>
2015-03-27 11:28:54 -0400

    mroe

--- Prolog/Learning/testprac.pl
+++ Prolog/Learning/testprac.pl
@@ -47,7 +47,8 @@ maxx([X|Xs],Max):-
 maxx([X|Xs], X):-
 	maxx(Xs,Max),
 	X > Max.
-	
+
+/*	
 set_difference([],[],[]).
 set_difference([X],[],[X]).
 set_difference([],[X],[X]).
@@ -57,5 +58,60 @@ set_difference([X|Xs],[Y|Ys],Result):-
 set_difference([X|Xs],[Y|Ys],Result):-
 	set_difference(Xs, Ys, Result]),
 	X \= Y.
+*/
 
-	
+blue(c,e).
+blue(d,e).
+
+green(a,d).
+green(b,c).
+green(b,e).
+green(e,d).
+
+connec(X,Y):-
+	blue(X,Z),
+	green(Z,Y).
+connec(X,Y):-
+	green(X,Z),
+	blue(Z,Y).
+
+shifr([X],[X]).
+shifr([X|Xs],New):-
+	append(Xs,[X],New).
+
+maxMark([],0).
+maxMark([project(_,_,M)],M).
+maxMark([project(_,_,M)|More], New):-
+	New > M,
+	maxMark(More,New).
+maxMark([project(_,_,M)|More], M):-
+	New < M,
+	maxMark(More,New).
+
+p(a,3).
+p(b,6).
+p(c,4).
+p(d,2).
+p(e,3).
+
+q(b,9).
+q(c,2).
+q(d,5).
+q(e,1).
+q(f,6).
+
+r(X):-
+	p(X,3).
+r(X):-
+	q(X,5).
+r(X):-
+	p(X,N),
+	q(X,D),
+	N>D.
+/*Remember to check all the elements of the second condition*/
+
+notthere(Y, [X]):-
+	Y \= X.
+notthere(Y, [X|Xs]):-
+	Y \= X,
+	notthere(Y, Xs).
