7fa8cd1370dc3716f3f36d1f01deb13215d3377a
Keith Weaver <keithmweaver@gmail.com>
2015-04-06 12:24:20 -0400

    added some comments.

--- Prolog/a3/assignment3.pl
+++ Prolog/a3/assignment3.pl
@@ -1,25 +1,26 @@
 /*
- *
- *
- *
- *
- *
- *
- *
+ * Assignment 3 from Prolog
+ * Keith Weaver
+ * 10048505
  */
 
+% the given time it takes per person
 family(original, [father/1, mother/2, child/5, granny/10]).
 
+% starter function. Passes in the starting list of people, to the move from
+% first shore to the second, it has to be less then maxtime
 moveFamily(FamilyName, MaxT, Moves, Time) :-
 	family(FamilyName, People),
 	moveToOtherShore(People, [], Moves, Time),
 	Time =< MaxT.
 
+% Passes in the people on both shores.
+% Passes in the Time
 moveBackToStart([],_,[],0).
 moveBackToStart(StartingShorePeople, OtherShorePeople, [Move|MoreMoves], Time) :-
-	member(Move/Time1, OtherShorePeople),
-	select(Move/Time1, OtherShorePeople, OtherShorePeopleRemaining),
-	moveToOtherShore([Move/Time1|StartingShorePeople], OtherShorePeopleRemaining, MoreMoves, Time2),
+	member(Move/Time1, OtherShorePeople),%checks to see if theyre a member
+	select(Move/Time1, OtherShorePeople, OtherShorePeopleRemaining),%removes the element from the othershore list
+	moveToOtherShore([Move/Time1|StartingShorePeople], OtherShorePeopleRemaining, MoreMoves, Time2),%adds it to the current shore (well the opposite, if that makes senses.)
 	Time is Time1+Time2.
 
 moveToOtherShore([],_,[],0).
